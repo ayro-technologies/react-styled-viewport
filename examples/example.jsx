d0984af9ae7c9a89a426fa8e32ca88b5ed6a392a
Eriomenco <Cristian.Eriomenco@experian.com>
2018-11-11 14:47:59 +0000

    Storybook wip

--- examples/example.jsx
+++ examples/example.jsx
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
 import { withMedia, MediaQuery } from '../src/index.js';
 import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 
 const query = new MediaQuery();
 const media = query.getMedia();
 
 const BtnWithoutProps = styled.button`
 
-  ${media.largeDesktop`font-size:180px;`}
-  ${media.desktop`font-size:80px;`}
-  ${media.tablet`font-size:40px;`}
-  ${media.mobile`font-size:20px;`}
+  ${media.largeDesktop`font-size:60px;`}
+  ${media.desktop`font-size:40px;`}
+  ${media.tablet`font-size:20px;`}
+  ${media.mobile`font-size:10px;`}
 
   ${media.lessThan(500)`background-color: blue;`}
   ${media.greaterThan(700)`background-color: red;`}
@@ -19,14 +20,17 @@ const BtnWithoutProps = styled.button`
 `;
 
 const BtnUsingProps = styled.button`
-  ${props => props.viewport.isLargeDesktop() && css`font-size:180px;`}
-  ${props => props.viewport.isDesktop() && css`font-size:80px;`}
-  ${props => props.viewport.isTablet() && css`font-size:40px;`}
-  ${props => props.viewport.isMobile() && css`font-size:20px;`}
+  ${props => props.viewport.isLargeDesktop() && css`font-size:60px;`}
+  ${props => props.viewport.isDesktop() && css`font-size:40px;`}
+  ${props => props.viewport.isTablet() && css`font-size:20px;`}
+  ${props => props.viewport.isMobile() && css`font-size:10px;`}
 `;
 
 
 storiesOf('Button', module)
+  .addDecorator(withInfo({
+    inline: true
+  }))
   .add('Using props', () => {
     const Btn = withMedia(BtnUsingProps);
     return <Btn>With props</Btn>;
